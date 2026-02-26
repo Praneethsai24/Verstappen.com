36cb7b6f205ae5a98dd8fe10669218abc84f0ed9
Praneethsai24 <spraneeth698@gmail.com>
2026-02-26 22:45:51 +0530

    This is a mess

--- script.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navList = document.querySelector(".nav-list");
-
-navToggle.addEventListener("click", () => {
-  navList.classList.toggle("active");
-});
